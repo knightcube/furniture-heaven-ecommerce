14b6775e529b9fe0850a7b268d327a6632fe6f87
Rajat Kumar Gupta <22871961+knightcube@users.noreply.github.com>
2023-10-16 14:07:45 +0530

    Added Sign in page

--- client/src/data/models/ProductModel.ts
+++ client/src/data/models/ProductModel.ts
@@ -2,13 +2,11 @@ class ProductModel{
     id: string
     name:string
     price:number
-    category:string
 
-    constructor(name:string,price:number, category:string){
+    constructor(name:string,price:number){
         this.id = new Date().toISOString()
         this.name = name
         this.price = price
-        this.category = category
     }
 }
 
